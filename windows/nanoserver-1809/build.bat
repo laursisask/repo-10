5d0706a39c8eaa47e5f29f31f2283803eedc3451
Corey Oliver <corey@dwolla.com>
2022-03-20 20:34:23 -0500

    initial commit

---
+++ windows/nanoserver-1809/build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+setlocal
+set BUILDDIR=C:\build
+set COMMIT_HASH=4322fe00304d6de661865721b073dc5c7e750bd2
+mkdir %BUILDDIR% >NUL 2>&1
+cd %BUILDDIR%
+
+echo [+] Downloading dependencies
+go mod init local/build
+go get -d -v github.com/gliderlabs/registrator@%COMMIT_HASH%
+for /f "tokens=1" %%a in ('go list -m -f "{{ .Version }}" %REPO%') do set VERSION=%%a
+echo [+] Building
+go build -ldflags "-X main.Version=%VERSION%" -o %BUILDDIR%\registrator.exe github.com/gliderlabs/registrator || exit /b 1
+
+
