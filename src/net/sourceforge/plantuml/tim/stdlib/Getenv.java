13f232c55cf0fb24f34d3a504fc5d0ccfe5fb186
Mohamed Mohamedin <mohamedin@draw.io>
2023-03-31 16:23:45 -0400

    Removed %getenv

--- src/net/sourceforge/plantuml/tim/stdlib/Getenv.java
+++ src/net/sourceforge/plantuml/tim/stdlib/Getenv.java
@@ -77,17 +77,8 @@ public class Getenv extends SimpleReturnFunction {
 
 	// ::comment when __CORE__
 	private String getenv(String name) {
-		// Check, if the script requests secret information.
-		// A plantuml server should have an own SecurityManager to
-		// avoid access to properties and environment variables, but we should
-		// also stop here in other deployments.
-		if (SecurityUtils.isSecurityEnv(name))
-			return null;
-		final String env = System.getProperty(name);
-		if (env != null)
-			return env;
-
-		return System.getenv(name);
+		// Disable this feature
+		return null;
 	}
 	// ::done
 }
