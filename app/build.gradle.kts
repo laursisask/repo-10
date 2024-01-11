0a312e4f1df0d4054fb62a8be361da02031a92c8
mranderson <askmranderson@gmail.com>
2024-01-11 15:23:37 +0100

    Convert gradle files to kts and use toml file

---
+++ app/build.gradle.kts
@@ -0,0 +1,97 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.secrets)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "nl.mranderson.rijks"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.2.0"
+    }
+}
+
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "local.defaults.properties"
+}
+
+dependencies {
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.paging)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.navigation)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.uiTooling)
+    implementation(libs.coilCompose)
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation)
+    implementation(libs.kotlinSerialization)
+    implementation(libs.lifecycle.compose)
+    implementation(libs.material)
+    implementation(libs.retrofit2)
+    implementation(libs.retrofitSerialization)
+
+    // Kapt Dependency
+    kapt(libs.hilt.workCompiler)
+
+    // Chucker Dependency
+    debugImplementation(libs.chucker)
+    releaseImplementation(libs.chucker)
+
+    // Test Dependencies
+    testImplementation(libs.androidx.arch.core)
+    testImplementation(libs.coroutinesTest)
+    testImplementation(libs.jupiter)
+    testImplementation(libs.jupiter.engine)
+    testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
+
+    // Android Test Dependencies
+    androidTestImplementation(libs.androidx.test)
+    androidTestImplementation(libs.androidx.test.compose.ui)
+    androidTestImplementation(libs.androidx.test.espresso)
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
