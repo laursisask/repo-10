debb66ae84305a365c4ec4fcb55159714caf0790
Alex Hung <alexh@jfrog.com>
2024-07-17 11:03:50 -0700

    Fix incorrect provider instance for testing
    Add sample.tf

--- pkg/myjfrog/util_test.go
+++ pkg/myjfrog/util_test.go
@@ -4,16 +4,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/jfrog/terraform-provider-platform/pkg/platform"
+	"github.com/jfrog/terraform-provider-myjfrog/pkg/myjfrog"
 )
 
 // TestProvider PreCheck(t) must be called before using this provider instance.
 var TestProvider provider.Provider
 
 func testAccProviders() map[string]func() (tfprotov6.ProviderServer, error) {
-	TestProvider = platform.NewProvider()()
+	TestProvider = myjfrog.NewProvider()()
 
 	return map[string]func() (tfprotov6.ProviderServer, error){
-		"platform": providerserver.NewProtocol6WithError(TestProvider),
+		"myjfrog": providerserver.NewProtocol6WithError(TestProvider),
 	}
 }
